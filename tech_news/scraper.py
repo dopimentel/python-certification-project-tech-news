1496e3714c510ec985f31fe5e39430cc1440d484
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-05-07 22:10:18 -0300

    feat: req1 function fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,23 @@
+import requests
+from time import sleep
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    headers = {
+        "User-Agent": "Fake user-agent",
+        # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+        # AppleWebKit/537.36 (KHTML, like Gecko)
+        # Chrome/58.0.3029.110 Safari/537.36"
+    }
+    try:
+        sleep(1)
+        response = requests.get(url, headers=headers, timeout=3)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print("Erro ao fazer requisição: ", e)
+        return None
+    return response.text
 
 
 # Requisito 2
