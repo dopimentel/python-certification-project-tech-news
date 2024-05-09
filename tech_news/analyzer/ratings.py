14cb99a0e30c53d5a2309ab95f8b27a4177493aa
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-05-08 21:08:59 -0300

    feat: req10 function top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    pipelines = [
+        {"$unwind": "$category"},
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+
+    query = db.news.aggregate(
+        pipelines,
+    )
+
+    categories = [category["_id"] for category in query]
+
+    return categories if categories else []
